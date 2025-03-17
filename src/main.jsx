eedb491512d4bfd0e46fbb252ccd612ecf914e10
Babuin4ikS <bytegeek33@gmail.com>
2025-03-17 12:59:39 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
-import { StrictMode } from 'react'
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+
 import DashboardLayoutBasic from './components/toolpad.jsx'
 
 createRoot(document.getElementById('main')).render(
